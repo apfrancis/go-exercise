d528c2b9f83b41279077ecbaa615033c03a3af5d
Andrew Francis <apfrancis@gmail.com>
2016-11-01 18:08:15 +0000

    refactor of original solution

--- ex005_make_friends/main.go
+++ ex005_make_friends/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
-	// your code here
+import "fmt"
+
+type person struct {
+	name string
+	friends []string
 }
+
+type friendship struct {
+	person1 string
+	person2 string
+}
+
+
+func main() {
+	akash := person{name:"akash"}
+	andy := person{name:"andy"}
+
+	andy.makeFriendsWith(akash)
+	andy.listFriends()
+
+}
+
+func (p *person) makeFriendsWith(friendee person)  {
+	p.friends = append(p.friends, friendee.name)
+}
+
+func (p person) listFriends() {
+	fmt.Println(p.friends)
+}
+
---
+++ ex005_make_friends/main_test.go
@@ -0,0 +1,1 @@
+package ex005_make_friends
